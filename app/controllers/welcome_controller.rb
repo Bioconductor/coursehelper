af36db8716b7400dd0208d416179a52f524b9a78
Dan Tenenbaum <dtenenba@fhcrc.org>
2016-01-20 09:12:39 -0800

    beter logic when user has already signed up

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -121,6 +121,13 @@ class WelcomeController < ApplicationController
       end
       email = params[:email].downcase
       #email.sub! "fhcrc.org", "fredhutch.org"
+      rec = Attendee.where(email: email).where(course_id: course.id).find_each do |user|
+        render("get_url_post", locals: {url: "http://#{rec.public_dns}",
+          enddate: course.enddate,
+          shellinabox_url: "http://#{rec.public_dns}:4200"}) and return
+      end        
+
+
       count = Attendee.where(course_id: course.id).length
       if count >= course.max_instances
         render(text: "Already started #{course.max_instances} instances for this course.") and return
